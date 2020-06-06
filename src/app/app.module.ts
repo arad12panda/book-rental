a519515f962c319689835f9b0570b2a47aa5a4a1
Aradhya <aaradhya.bhushan12@gmail.com>
2020-06-06 19:44:02 +0530

    added service model routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/header.component';
+import { AppComponent } from './components/app/app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { RentalModule } from 'src/modules/rental/rental.module';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
-  ],
+    HeaderComponent,
+    ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RentalModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
